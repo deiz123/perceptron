8360e31ad22c8abdc22eb6171a47e8ea74b2c760
kirill.nivin <kirill.nivin@team.wrike.com>
2020-05-26 00:04:20 +0300

    init

---
+++ script/main.js
@@ -0,0 +1,155 @@
+var defaultLayers = {"Layers":[{"Neurons":[{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1},{"Dendrites":[],"Weight":-1}]},{"Neurons":[]}]};
+
+
+$(document).ready(function () {
+    var mlp = new MLP(defaultLayers);
+
+    context = document.getElementById("canvas").getContext("2d");
+
+    outlineContext = document.getElementById("outlines").getContext("2d");
+    outlineContext.fillStyle = "red";
+    outlineContext.strokeStyle = "gray";
+
+    $('#canvas').mousedown(function (e) {
+        var mouseX = e.pageX - this.offsetLeft;
+        var mouseY = e.pageY - this.offsetTop;
+
+        paint = true;
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+        redraw();
+    }).mousemove(function (e) {
+        if (paint) {
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+            redraw();
+        }
+    }).mouseup(function (e) {
+        paint = false;
+    }).mouseleave(function (e) {
+        paint = false;
+    });
+
+    $("#clear").on("click", function () {
+        //clear drawing context
+        clickX = [];
+        clickY = [];
+        clickDrag = [];
+        paint = false;
+        redraw();
+
+        //clear outline context
+        outlineContext.clearRect(0, 0, outlineContext.canvas.width, outlineContext.canvas.height);
+
+        //clear the guess
+        $("#bestGuess").text("");
+        $("#outputValues").html("");
+    });
+
+    $("#read").on("click", function () {
+        var drawing = getDownsampledDrawing();
+
+        var inputLayer = mlp.Layers[0];
+        var outputLayer = mlp.Layers[mlp.Layers.length - 1];
+
+        for (var i = 0; i < drawing.length; i++) {
+            inputLayer.Neurons[i].Weight = drawing[i];
+        }
+
+        mlp.Think();
+
+        $("#outputValues").html("<h3>Выходные значения</h3>");
+        for (var i = 0; i < outputLayer.Neurons.length; i++) {
+            var neuron = outputLayer.Neurons[i];
+            $("#outputValues").append("<span>" + neuron.Name.toUpperCase() + ": " + neuron.Weight + "</span>");
+        }
+        var bestGuess = outputLayer.BestGuess();
+        if (bestGuess !== null) {
+            $("#bestGuess").text(bestGuess.Name.toUpperCase());
+        } else {
+            $("#outputValues").html("Не удалось распознать. пусто");
+        }
+    });
+
+    $("#trainCharacter").on("click", function () {
+        var inputLayer = mlp.Layers[0];
+        var outputLayer = mlp.Layers[mlp.Layers.length - 1];
+
+        //determine the character to be trained
+        var character = $("#txtCharacter").val().toUpperCase();
+        if (character.length === 0) {
+            //no character, no glory
+            return;
+        }
+
+        //lookup the output neuron for the character to train
+        var outputNeuron = outputLayer.GetNeuron(character);
+
+        //if the character has not been trained before, add it to the output layer
+        if (outputNeuron === null) {
+            var outputNeuron = new Neuron(character)
+            inputLayer.ConnectNeuron(outputNeuron);
+            outputLayer.Neurons.push(outputNeuron);
+        }
+
+        //train the network with the current drawing applied to the output neuron
+        mlp.Train(getDownsampledDrawing(), outputNeuron);
+    });
+
+    var clickX = [];
+    var clickY = [];
+    var clickDrag = [];
+    var paint;
+
+    function addClick(x, y, dragging) {
+        clickX.push(x);
+        clickY.push(y);
+        clickDrag.push(dragging);
+    }
+
+    function redraw() {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+
+        context.strokeStyle = "black";
+        context.lineJoin = "round";
+        context.lineWidth = 5;
+
+        for (var i = 0; i < clickX.length; i++) {
+            context.beginPath();
+            if (clickDrag[i] && i) {
+                context.moveTo(clickX[i - 1], clickY[i - 1]);
+            } else {
+                context.moveTo(clickX[i] - 1, clickY[i]);
+            }
+            context.lineTo(clickX[i], clickY[i]);
+            context.closePath();
+            context.stroke();
+        }
+    }
+});
+
+function getDownsampledDrawing() {
+    var output = [];
+    var blockSize = 10;
+    var canvasSizeX = context.canvas.width;
+    var canvasSizeY = context.canvas.height;
+
+    for (var x = 0; x < canvasSizeX; x += blockSize) {
+        for (var y = 0; y < canvasSizeY; y += blockSize) {
+            var data = context.getImageData(x, y, blockSize, blockSize).data;
+            var found = false;
+            for (var i = 0; i < data.length; i++) {
+                if (data[i]) {
+                    output.push(1);
+                    outlineContext.fillRect(x, y, blockSize, blockSize);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                output.push(-1);
+            }
+            outlineContext.strokeRect(x, y, blockSize, blockSize);
+        }
+    }
+
+    return output;
+}
